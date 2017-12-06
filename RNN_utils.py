59d8280f68ecf67646db4578707130da820f60e8
Markus Kängsepp <markus9696@gmail.com>
2017-12-06 16:28:14 +0200

    Add basic scripts to Test Tensorport

---
+++ RNN_utils.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+# method for generating text, using model
+# TODO add randomness to output.
+def generate_text(model, length, vocab_size, ix_to_char):
+	# starting with random character
+	ix = [np.random.randint(vocab_size)]
+	y_char = [ix_to_char[ix[-1]]]
+	X = np.zeros((1, length, vocab_size))
+	for i in range(length):
+		# appending the last predicted character to sequence
+		X[0, i, :][ix[-1]] = 1
+		#ix_to_char[ix[-1]], end=""
+		ix = np.argmax(model.predict(X[:, :i+1, :])[0], 1)
+		y_char.append(ix_to_char[ix[-1]])
+	return ('').join(y_char)
+
+# Read data and generate vocabulary    
+def load_vocabulary(data_dir, seq_length, batch_size):
+    data = open(data_dir, 'r').read()  # Read data
+    chars = list(set(data))  # get possible chars
+    VOCAB_SIZE = len(chars)
+
+    print('Data length: {} characters'.format(len(data)))
+    print('Vocabulary size: {} characters'.format(VOCAB_SIZE))
+
+    ix_to_char = {ix:char for ix, char in enumerate(chars)}  # index to char map
+    char_to_ix = {char:ix for ix, char in enumerate(chars)}  # char to index map
+    
+    steps_per_epoch = len(data)//seq_length//batch_size
+    
+    return VOCAB_SIZE, ix_to_char, char_to_ix, steps_per_epoch
+    
+# Read in data by batches, atm only for char-to-char
+def data_generator(data_dir, seq_length, batch_size, steps_per_epoch):
+    data = open(data_dir, 'r').read()  # Read data
+    chars = list(set(data))  # get possible chars
+    VOCAB_SIZE = len(chars)
+
+    print('Data length: {} characters'.format(len(data)))
+    print('Vocabulary size: {} characters'.format(VOCAB_SIZE))
+
+    ix_to_char = {ix:char for ix, char in enumerate(chars)}  # index to char map
+    char_to_ix = {char:ix for ix, char in enumerate(chars)}  # char to index map
+    
+    batch_nr = 0
+    
+    while True:
+        
+        X = np.zeros((batch_size, seq_length, VOCAB_SIZE))  # input data
+        y = np.zeros((batch_size, seq_length, VOCAB_SIZE))
+        
+        pos_start = batch_nr*batch_size*seq_length  # Continue where left on from patch
+        
+        for i in range(0, batch_size):        
+            
+            X_sequence = data[pos_start + i*seq_length:pos_start + (i+1)*seq_length]
+            X_sequence_ix = [char_to_ix[value] for value in X_sequence]
+            input_sequence = np.zeros((seq_length, VOCAB_SIZE))
+
+            for j in range(len(X_sequence)):  # Last sequence otherwise shorter
+                input_sequence[j][X_sequence_ix[j]] = 1.
+                X[i] = input_sequence
+
+            y_sequence = data[pos_start+i*seq_length+1:pos_start + (i+1)*seq_length+1]  # next character, as we want to predict next character
+            y_sequence_ix = [char_to_ix[value] for value in y_sequence]
+            target_sequence = np.zeros((seq_length, VOCAB_SIZE))
+            
+            for j in range(len(y_sequence)):
+                target_sequence[j][y_sequence_ix[j]] = 1.
+                y[i] = target_sequence
+        
+        if batch_nr == (steps_per_epoch-1):  # Because we start from zero
+            batch_nr = 0  # Back to beginning - so we could loop indefinitely
+        else:
+            batch_nr += 1
+                
+        
+        yield(X, y)
---
+++ main_lstm.py
@@ -0,0 +1,70 @@
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.wrappers import TimeDistributed
+from RNN_utils import *
+
+def main():
+
+    DATA_DIR = "poems_test_small.txt"
+    BATCH_SIZE = 100
+    HIDDEN_DIM = 500
+    SEQ_LENGTH = 100
+    WEIGHTS = ""
+    MODE = "train"
+    DROPOUT_RATE = 0.2
+    GENERATE_LENGTH = 500
+    LAYER_NUM = 2
+    EPOCHS = 40
+
+
+    print("Weights:", WEIGHTS)
+    print("Mode:", MODE)
+
+
+    # Find VOCAB_SIZE and dictionaries
+    VOCAB_SIZE, ix_to_char, char_to_ix, steps_per_epoch = load_vocabulary(DATA_DIR, SEQ_LENGTH, BATCH_SIZE)
+
+    # Creating and compiling the Network
+    model = Sequential()
+    model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
+    model.add(Dropout(rate = DROPOUT_RATE))
+    for i in range(LAYER_NUM - 1):
+      model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+      model.add(Dropout(rate = DROPOUT_RATE))
+    model.add(TimeDistributed(Dense(VOCAB_SIZE)))
+    model.add(Activation('softmax'))
+    model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+    # Load model if file for weights is given
+    if not WEIGHTS == '':
+      model.load_weights(WEIGHTS)
+      epoch = int(WEIGHTS[WEIGHTS.rfind('_') + 1:WEIGHTS.find('.')])
+    else:
+      epoch = 0
+
+    # Training
+    if MODE == 'train':
+      while True:
+        print('\n\nEpoch: {}\n'.format(epoch))
+        model.fit_generator(data_generator(DATA_DIR, SEQ_LENGTH, BATCH_SIZE, steps_per_epoch), \
+        steps_per_epoch=steps_per_epoch, verbose = 1, epochs = 1)
+        epoch += 1
+        #generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+        if epoch % 10 == 0:
+          model.save_weights('checkpoint_layer_{}_hidden_{}_epoch_{}.hdf5'.format(LAYER_NUM, HIDDEN_DIM, epoch))
+
+    # Else, loading the trained weights and performing generation only
+    elif WEIGHTS != '':
+      # Loading the trained weights
+      #model.load_weights(WEIGHTS)
+      print("Translating")
+      for i in range(5):
+        print(generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char))
+        print('\n\n')
+    else:
+      print('\n\nNothing to do!')
+
+# Call out main function
+if __name__ == "__main__":
+    main()
