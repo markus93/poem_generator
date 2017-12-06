c47d94dd5adc7991899f8132885907db31755225
Markus Kängsepp <markus9696@gmail.com>
2017-12-06 20:41:49 +0200

    Fix data directory path for Tensorflow. also adjust dropout rates.

--- scripts/main_100k_3layers.py
+++ scripts/main_100k_3layers.py
@@ -5,5 +5,5 @@ from scripts.lstm import *
 # Call out main function
 if __name__ == "__main__":
     print("Start training")
-    lstm(data_dir = "../data/poets_top_100k.txt", layer_num=3)
+    lstm(data_dir = "data/poets_top_100k.txt", layer_num=3, dropout_rate=0.1)
     print("Finished")
